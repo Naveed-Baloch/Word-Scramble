5616e83557a7976b32790d22c614b7d5bbd0380d
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-16 20:27:39 +0500

    Initial Commit

---
+++ Word Scramble/Word_ScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Word_ScrambleApp.swift
+//  Word Scramble
+//
+//  Created by Naveed on 16/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct Word_ScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
