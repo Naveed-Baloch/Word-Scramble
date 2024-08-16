f1116f8901f34bdab3f196cd88c782d444fda3e5
Muhammad Naveed <muhammad.naveed@alliedc.com>
2024-08-16 20:35:25 +0500

    use dynamic list users

--- Word Scramble/ContentView.swift
+++ Word Scramble/ContentView.swift
@@ -8,28 +8,11 @@
 import SwiftUI
 
 struct ContentView: View {
+    let users = ["Naveed", "Ali","Burhan"]
     var body: some View {
-        List {
-            Section("Section 1") {
-                Text("Static row 1")
-                Text("Static row 2")
-            }
-
-            Section("Section 2") {
-                ForEach(0..<5) { number in
-                    Section(String(number)){
-                        Text("Dynamic row \(number)")
-                    }
-
-                }
-            }
-
-            Section("Section 3") {
-                Text("Static row 3")
-                Text("Static row 4")
-            }
+        List(users, id: \.self){
+            Text("\($0)")
         }
-        .listStyle(.insetGrouped)
     }
 }
 
